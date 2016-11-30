84145beda80ce32bd26d6c05b707729b9f5160e6
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-11-30 15:01:33 -0500

    added skeleton for classyworker

---
+++ classy/classyworker.py
@@ -0,0 +1,8 @@
+'''classyworker.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Nov 2016
+
+Licence: MIT. See LICENSE for details.
+
+This is the worker process that handles requests from the classy-web frontend
+server (implemented in classyweb.py).
+
+'''
