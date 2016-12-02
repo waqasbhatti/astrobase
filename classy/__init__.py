e2a458a9ce539d762694cc52efc39df15b37f2f9
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-12-02 11:08:00 -0500

    removed classy

--- classy/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-varclass - Waqas Bhatti (wbhatti@astro.princeton.edu) - Nov 2016
-
-License: MIT. See LICENSE for complete text.
-
-This is a web-app to interactively classify variable stars.
-
-'''
--- classy/classyworker.py
+++
@@ -1,8 +0,0 @@
-'''classyworker.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Nov 2016
-
-Licence: MIT. See LICENSE for details.
-
-This is the worker process that handles requests from the classy-web frontend
-server (implemented in classyweb.py).
-
-'''
