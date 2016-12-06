f402ee196de8a50ce2560eea2fff6de496b2962b
Waqas Bhatti <wbhatti@astro.princeton.edu>
2016-12-06 12:50:18 -0500

    setup.py now works using numpy's setup function. very janky...

--- setup.py
+++ setup.py
@@ -13,23 +13,7 @@ import sys, os.path
 from setuptools import setup
 
 # for f2py extension building
-from numpy.distutils.core import Extension
-
-# taken from github:dfm/python-bls.git/setup.py
-# First, make sure that the f2py interfaces exist.
-interface_exists = os.path.exists("bls/bls.pyf")
-if "interface" in sys.argv or not interface_exists:
-    # Generate the Fortran signature/interface.
-    cmd = "cd bls;"
-    cmd += "f2py eebls.f -m _bls -h bls.pyf"
-    cmd += " --overwrite-signature"
-    os.system(cmd)
-    if "interface" in sys.argv:
-        sys.exit(0)
-
-# Define the Fortran extension.
-bls = Extension("bls._bls", ["bls/bls.pyf", "bls/eebls.f"])
-
+from numpy.distutils.core import Extension, setup as npsetup
 
 def readme():
     with open('README.md') as f:
@@ -53,7 +37,7 @@ setup(
     author='Waqas Bhatti',
     author_email='waqas.afzal.bhatti@gmail.com',
     license='MIT',
-    packages=['astrobase','bls'],
+    packages=['astrobase'],
     install_requires=[
         'numpy',
         'scipy',
@@ -74,4 +58,49 @@ setup(
     #},
     include_package_data=True,
     zip_safe=False,
+)
+
+
+############################
+## NOW DO THE FORTRAN BIT ##
+############################
+
+# taken from github:dfm/python-bls.git/setup.py
+# First, make sure that the f2py interfaces exist.
+interface_exists = os.path.exists("bls/bls.pyf")
+if "interface" in sys.argv or not interface_exists:
+    # Generate the Fortran signature/interface.
+    cmd = "cd bls;"
+    cmd += "f2py eebls.f -m _bls -h bls.pyf"
+    cmd += " --overwrite-signature"
+    os.system(cmd)
+    if "interface" in sys.argv:
+        sys.exit(0)
+
+# Define the Fortran extension.
+bls = Extension("bls._bls", ["bls/bls.pyf", "bls/eebls.f"])
+
+npsetup(
+    name='bls',
+    version=__version__,
+    description=('Python f2py extension wrapping '
+                 'eebls.f by Kovacs et al. 2002.'),
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='astronomy',
+    url='https://github.com/waqasbhatti/astrobase',
+    author='Waqas Bhatti',
+    author_email='waqas.afzal.bhatti@gmail.com',
+    license='MIT',
+    packages=["bls"],
+    ext_modules=[bls,],
+    install_requires=[
+        'numpy',
+    ],
 )
