79a82fa55c23480433aa52099a418edf5f402c10
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-05-03 16:23:58 -0400

    working on adding a flare detector

---
+++ astrobase/varbase/flares.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+
+'''flares.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - May 2017
+
+Contains functions to deal with finding stellar flares in time series.
+
+'''
+
+
+import logging
+from datetime import datetime
+from traceback import format_exc
+from time import time as unixtime
+import os.path
+import os
+
+from numpy import nan as npnan, sum as npsum, abs as npabs, \
+    roll as nproll, isfinite as npisfinite, std as npstd, \
+    sign as npsign, sqrt as npsqrt, median as npmedian, \
+    array as nparray, percentile as nppercentile, \
+    polyfit as nppolyfit, var as npvar, max as npmax, min as npmin, \
+    log10 as nplog10, arange as nparange, pi as MPI, floor as npfloor, \
+    argsort as npargsort, cos as npcos, sin as npsin, tan as nptan, \
+    where as npwhere, linspace as nplinspace, \
+    zeros_like as npzeros_like, full_like as npfull_like, all as npall, \
+    correlate as npcorrelate
+
+from scipy.signal import savgol_filter
+
+###################
+## LOCAL IMPORTS ##
+###################
+
+
+
+#############
+## LOGGING ##
+#############
+
+# setup a logger
+LOGGER = None
+
+def set_logger_parent(parent_name):
+    globals()['LOGGER'] = logging.getLogger('%s.varbase.flares' % parent_name)
+
+def LOGDEBUG(message):
+    if LOGGER:
+        LOGGER.debug(message)
+    elif DEBUG:
+        print('%sZ [DBUG]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGINFO(message):
+    if LOGGER:
+        LOGGER.info(message)
+    else:
+        print('%sZ [INFO]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGERROR(message):
+    if LOGGER:
+        LOGGER.error(message)
+    else:
+        print('%sZ [ERR!]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGWARNING(message):
+    if LOGGER:
+        LOGGER.warning(message)
+    else:
+        print('%sZ [WRN!]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGEXCEPTION(message):
+    if LOGGER:
+        LOGGER.exception(message)
+    else:
+        print(
+            '%sZ [EXC!]: %s\nexception was: %s' % (
+                datetime.utcnow().isoformat(),
+                message, format_exc()
+                )
+            )
+
+
+
+###########################
+## FLARE MODEL FUNCTIONS ##
+###########################
+
+def flare_model(times,
+                mags,
+                errs,
+                amplitude,
+                flare_peak_time,
+                rise_gaussian_stdev,
+                decay_time_constant,
+                magsarefluxes=False):
+    '''This is a flare model function, similar to Kowalski+ 2011.
+
+    From the paper by Pitkin+ 2014:
+
+    http://adsabs.harvard.edu/abs/2014MNRAS.445.2268P
+
+    times: a numpy array of times. the flare model will be interpolated to match
+    the cadence and gaps in this data
+
+    mags: a numpy array of magnitudes or fluxes. the median level will be used
+    as the base for the flare model.
+
+    errs: a numpy array of measurement errors for each mag/flux measurement
+
+    amplitude: the maximum flare amplitude in mags or flux
+
+    flare_peak_time: time at which the flare maximum happens
+
+    rise_gaussian_stdev: the stdev of the gaussian describing the rise of the
+                         flare
+
+    decay_time_constant: the time constant of the exponential fall of the flare
+
+    If magsarefluxes = True: everything will be assumed to be in fluxes.
+
+    '''
+
+    internal_times = np.linspace(times.nanmin(), times.nanmax(), num=1000)
+    base_level = npmedian(mags)
+
+    # set up the model
+    if magsarefluxes:
+
+        blah()
+
+    else:
+
+        bleh()
+
+
+
+###################
+## FLARE FINDERS ##
+###################
+
+def simple_flare_find(times, mags, errs,
+                      smoothbinsize=97,
+                      flareminsigma=4.0,
+                      flaremaxcadencediff=1,
+                      flaremincadencepoints=3,
+                      magsarefluxes=False,
+                      savgolpolyorder=2,
+                      **savgolkwargs):
+    '''This finds flares in  time series using the method in Walkowicz+ 2011.
+
+    Returns number of flares found, and their time indices.
+
+    Args
+    ----
+
+    times, mags, errs are numpy arrays for the time series.
+
+    Kwargs
+    ------
+
+    smoothbinsize: the number of consecutive light curve points to smooth over
+    in the time series using a Savitsky-Golay filter. The smoothed light curve
+    is then subtracted from the actual light curve to remove trends that
+    potentially last smoothbinsize light curve points. The default value is
+    chosen as ~6.5 hours (97 x 4 minute cadence for HATNet/HATSouth).
+
+    flareminsigma: the minimum sigma above the median light curve level to
+    designate points as belonging to possible flares
+
+    flaremaxcadencediff: the maximum number of light curve points apart each
+    possible flare event measurement is allowed to be. If this is 1, then we'll
+    look for consecutive measurements.
+
+    flaremincadencepoints: the minimum number of light curve points (each
+    flaremaxcadencediff points apart) required that are at least flareminsigma
+    above the median light curve level to call an event a flare.
+
+    magsarefluxes: if True, indicates that mags is actually an array of fluxes.
+
+    savgolpolyorder: the polynomial order of the function used by the
+    Savitsky-Golay filter.
+
+    Any remaining keyword arguments are passed directly to the savgol_filter
+    function from scipy.
+
+    '''
+
+    # if no errs are given, assume 0.1% errors
+    if errs is None:
+        errs = 0.001*mags
+
+    # get rid of nans first
+    finiteind = npisfinite(times) & npisfinite(mags) & npisfinite(errs)
+    ftimes = times[finiteind]
+    fmags = mags[finiteind]
+    ferrs = errs[finiteind]
+
+    # now get the smoothed mag series using the filter
+    # kwargs are provided to the savgol_filter function
+    smoothed = savgol_filter(fmags,
+                             smoothbinsize,
+                             savgolpolyorder,
+                             **savgolkwargs)
+    subtracted = fmags - smoothed
+
+    # calculate some stats
+    # the series_median is ~zero after subtraction
+    series_mad = npmedian(npabs(subtracted))
+    series_stdev = 1.483*series_mad
+
+    # find extreme positive deviations
+    if magsarefluxes:
+        extind = npwhere(subtracted > (minflaresigma*series_stdev))
+    else:
+        extind = npwhere(subtracted < (-minflaresigma*series_stdev))
+
+
+    # figure out if there are any consecutive deviations
+    if extind and extind[0]:
+
+        extind = extind[0]
+        diffextind = np.diff(extind)
+
+        # find the deviations within the requested flaremaxcadencediff
+        di
+
+
+
+
+############################
+## FLARE CHARACTERIZATION ##
+############################
