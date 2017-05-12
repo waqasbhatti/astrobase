hplc: fixing normalization

astrobase/hplc.py
@@ -265,7 +265,8 @@ def read_hatpi_pklc(lcfile):
 
 def concatenate_textlcs(lclist,
                         sortby='rjd',
-                        normalize=True):
+                        normalize=True,
+                        normto=None):
     '''This concatenates a list of light curves.
 
     Does not care about overlaps or duplicates. The light curves must all be
@@ -281,6 +282,9 @@ def concatenate_textlcs(lclist,
     If normalize is True, then each light curve's magnitude columns are
     normalized to zero, and the whole light curve is then normalized to the
     global median magnitude for each magnitude column.
+
+    If normto == 'globalmedian', will add back the global median values to the
+    light curve.
 
     '''
 
@@ -381,18 +385,20 @@ def concatenate_textlcs(lclist,
                         thismedval
                     )
 
-        # now that everything is normalized to zero, we need to add back the
-        # global median value for each column across all light curves
-        for col in MAGCOLS:
 
-            # handle fluxes
-            if col in ('ifl1','ifl2','ifl3'):
+        if normto == 'globalmedian':
+            # now that everything is normalized to zero, we need to add back the
+            # global median value for each column across all light curves
+            for col in MAGCOLS:
 
-                lcdict[col] = lcdict[col] * np.median(medianmags[col])
+                # handle fluxes
+                if col in ('ifl1','ifl2','ifl3'):
 
-            else:
+                    lcdict[col] = lcdict[col] * np.median(medianmags[col])
 
-                lcdict[col] = lcdict[col] + np.median(medianmags[col])
+                else:
+
+                    lcdict[col] = lcdict[col] + np.median(medianmags[col])
 
 
     LOGINFO('done. concatenated light curve has %s detections' %
@@ -405,7 +411,8 @@ def concatenate_textlcs_for_objectid(lcbasedir,
                                      objectid,
                                      aperture='TF1',
                                      sortby='rjd',
-                                     normalize=True):
+                                     normalize=True,
+                                     normto=None):
     '''This concatenates all text LCs for an objectid with the given aperture.
 
     Does not care about overlaps or duplicates. The light curves must all be
@@ -430,6 +437,9 @@ def concatenate_textlcs_for_objectid(lcbasedir,
     If normalize is True, then each light curve's magnitude columns are
     normalized to zero, and the whole light curve is then normalized to the
     global median magnitude for each magnitude column.
+
+    If normto == 'globalmedian', will add back the global median values to the
+    light curve.
 
     '''
     LOGINFO('looking for light curves for %s, aperture %s in directory: %s'
@@ -467,7 +477,8 @@ def concatenate_textlcs_for_objectid(lcbasedir,
     if matching and len(matching) > 1:
         clcdict = concatenate_textlcs(matching,
                                       sortby=sortby,
-                                      normalize=normalize)
+                                      normalize=normalize,
+                                      normto=normto)
         return clcdict
     elif matching and len(matching) == 1:
         return read_hatpi_textlc(matching[0])