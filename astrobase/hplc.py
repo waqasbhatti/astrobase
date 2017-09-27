3e573fa741a5d4c9b5261249b3219a2b8b22971d
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-09-27 17:45:48 -0400

    hplc: added parallel LC concatenator

--- astrobase/hplc.py
+++ astrobase/hplc.py
@@ -581,6 +581,66 @@ def concat_write_pklc(lcbasedir,
     return pklc
 
 
+
+def parallel_concat_worker(task):
+    '''
+    This is a worker for the function below.
+
+    task[0] = lcbasedir
+    task[1] = objectid
+    task[2] = {'aperture','postfix','sortby','normalize','outdir','recursive'}
+
+    '''
+
+    lcbasedir, objectid, kwargs = task
+
+    try:
+        return concat_write_pklc(lcbasedir, objectid, **kwargs)
+    except Exception as e:
+        LOGEXCEPTION('failed LC concatenation for %s in %s'
+                     % (objectid, lcbasedir))
+        return None
+
+
+
+def parallel_concat_lcdir(lcbasedir,
+                          objectidlist,
+                          aperture='TF1',
+                          postfix='.gz',
+                          sortby='rjd',
+                          normalize=True,
+                          outdir=None,
+                          recursive=True,
+                          nworkers=32,
+                          maxworkertasks=1000):
+    '''This concatenates all text LCs for the given objectidlist.
+
+
+    '''
+
+    if not outdir:
+        outdir = 'pklcs'
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    tasks = [(lcbasedir, x, {'aperture':aperture,
+                             'postfix':postfix,
+                             'sortby':sortby,
+                             'normalize':normalize,
+                             'outdir':outdir,
+                             'recursive':recursive}) for x in objectidlist]
+
+    pool = mp.Pool(nworkers, maxtasksperchild=maxworkertasks)
+    results = pool.map(parallel_concat_worker, tasks)
+
+    pool.close()
+    pool.join()
+
+    return {x:y for (x,y) in zip(objectidlist, results)}
+
+
+
 def pklc_fovcatalog_objectinfo(
         pklcdir,
         fovcatalog,
