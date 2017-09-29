c25401905683990b8401491c7cc401b99c4ed68b
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-09-29 14:24:40 -0400

    hplc: working on binnedlc collection

--- astrobase/hplc.py
+++ astrobase/hplc.py
@@ -823,7 +823,24 @@ def parallel_gen_binnedlc_pkls(binnedpkldir,
 
     '''
 
-    binnedpkls = glob.glob(os.path.join(binnedpkldir, binnedpklglob))
+    binnedpkls = sorted(glob.glob(os.path.join(binnedpkldir, binnedpklglob)))
+
+    # find all the textlcs associated with these
+    textlcs = []
+
+    for bpkl in binnedpkls:
+
+        objectid = HATIDREGEX.findall(bpkl)
+        if objectid is not None:
+            objectid = objectid[0]
+
+        searchpath = os.path.join(textlcdir, '%s-%s' % (objectid, textlcglob))
+        textlcf = glob.glob(searchpath)
+        if textlcf:
+            textlcs.append(textlcf)
+        else:
+            textlcs.append(None)
+
 
 
 
