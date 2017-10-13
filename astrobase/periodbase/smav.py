0150a9d9b2f92fe9a71c8019e3df7be8662bc5ee
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2017-10-12 23:16:09 -0400

    smav: implemented aovhm_periodfind, to be tested

--- astrobase/periodbase/smav.py
+++ astrobase/periodbase/smav.py
@@ -101,7 +101,7 @@ NCPUS = cpu_count()
 ###################################################################
 
 
-def harmonic_aov_theta(times, mags, errs, frequency,
+def aovhm_theta(times, mags, errs, frequency,
                        nharmonics, magvariance):
     '''This calculates the harmonic AoV theta for a frequency.
 
@@ -208,3 +208,249 @@ def harmonic_aov_theta(times, mags, errs, frequency,
                                             1.0e-9])) )
 
     return theta_aov
+
+
+
+def aovhm_theta_worker(task):
+    '''
+    This is a parallel worker for the function below.
+
+    task[0] = times
+    task[1] = mags
+    task[2] = errs
+    task[3] = frequency
+    task[4] = nharmonics
+    task[5] = magvariance
+
+    '''
+
+    times, mags, errs, frequency, nharmonics, magvariance = task
+
+    try:
+
+        theta = aovhm_theta(times, mags, errs, frequency,
+                            nharmonics, magvariance)
+
+        return theta
+
+    except Exception as e:
+
+        return npnan
+
+
+
+def aovhm_periodfind(times,
+                     mags,
+                     errs,
+                     nharmonics=6,
+                     magsarefluxes=False,
+                     autofreq=True,
+                     startp=None,
+                     endp=None,
+                     normalize=True,
+                     stepsize=1.0e-4,
+                     nbestpeaks=5,
+                     periodepsilon=0.1, # 0.1
+                     sigclip=10.0,
+                     nworkers=None,
+                     verbose=True):
+    '''This runs a parallel AoV period search.
+
+    NOTE: normalize = True here as recommended by Schwarzenberg-Czerny 1996,
+    i.e. mags will be normalized to zero and rescaled so their variance = 1.0
+
+    '''
+
+    # get rid of nans first and sigclip
+    stimes, smags, serrs = sigclip_magseries(times,
+                                             mags,
+                                             errs,
+                                             magsarefluxes=magsarefluxes,
+                                             sigclip=sigclip)
+
+    # make sure there are enough points to calculate a spectrum
+    if len(stimes) > 9 and len(smags) > 9 and len(serrs) > 9:
+
+        # get the frequencies to use
+        if startp:
+            endf = 1.0/startp
+        else:
+            # default start period is 0.1 day
+            endf = 1.0/0.1
+
+        if endp:
+            startf = 1.0/endp
+        else:
+            # default end period is length of time series
+            startf = 1.0/(stimes.max() - stimes.min())
+
+        # if we're not using autofreq, then use the provided frequencies
+        if not autofreq:
+            frequencies = np.arange(startf, endf, stepsize)
+            if verbose:
+                LOGINFO(
+                    'using %s frequency points, start P = %.3f, end P = %.3f' %
+                    (frequencies.size, 1.0/endf, 1.0/startf)
+                )
+        else:
+            # this gets an automatic grid of frequencies to use
+            frequencies = get_frequency_grid(stimes,
+                                             minfreq=startf,
+                                             maxfreq=endf)
+            if verbose:
+                LOGINFO(
+                    'using autofreq with %s frequency points, '
+                    'start P = %.3f, end P = %.3f' %
+                    (frequencies.size,
+                     1.0/frequencies.max(),
+                     1.0/frequencies.min())
+                )
+
+        # map to parallel workers
+        if (not nworkers) or (nworkers > NCPUS):
+            nworkers = NCPUS
+            if verbose:
+                LOGINFO('using %s workers...' % nworkers)
+
+        pool = Pool(nworkers)
+
+        # renormalize the working mags to zero and scale them so that the
+        # variance = 1 for use with our LSP functions
+        if normalize:
+            nmags = (smags - npmedian(smags))/npstd(smags)
+        else:
+            nmags = smags
+
+        # figure out the weighted variance
+        # www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weighvar.pdf
+        magvariance_top = npsum(nmags/(errs*errs))
+        magvariance_bot = (nmags.size - 1)*npsum(1.0/(errs*errs)) / nmags.size
+        magvariance = magvariance_top/magvariance_bot
+
+        tasks = [(stimes, nmags, serrs, x, nharmonics, magvariance)
+                 for x in frequencies]
+
+        lsp = pool.map(aovhm_worker, tasks)
+
+        pool.close()
+        pool.join()
+        del pool
+
+        lsp = nparray(lsp)
+        periods = 1.0/frequencies
+
+        # find the nbestpeaks for the periodogram: 1. sort the lsp array by
+        # highest value first 2. go down the values until we find five
+        # values that are separated by at least periodepsilon in period
+
+        # make sure to filter out non-finite values
+        finitepeakind = npisfinite(lsp)
+        finlsp = lsp[finitepeakind]
+        finperiods = periods[finitepeakind]
+
+        # make sure that finlsp has finite values before we work on it
+        try:
+
+            bestperiodind = npargmax(finlsp)
+
+        except ValueError:
+
+            LOGERROR('no finite periodogram values '
+                     'for this mag series, skipping...')
+            return {'bestperiod':npnan,
+                    'bestlspval':npnan,
+                    'nbestpeaks':nbestpeaks,
+                    'nbestlspvals':None,
+                    'nbestperiods':None,
+                    'lspvals':None,
+                    'periods':None,
+                    'method':'aov',
+                    'kwargs':{'startp':startp,
+                              'endp':endp,
+                              'stepsize':stepsize,
+                              'normalize':normalize,
+                              'phasebinsize':phasebinsize,
+                              'mindetperbin':mindetperbin,
+                              'autofreq':autofreq,
+                              'periodepsilon':periodepsilon,
+                              'nbestpeaks':nbestpeaks,
+                              'sigclip':sigclip}}
+
+        sortedlspind = np.argsort(finlsp)[::-1]
+        sortedlspperiods = finperiods[sortedlspind]
+        sortedlspvals = finlsp[sortedlspind]
+
+        prevbestlspval = sortedlspvals[0]
+        # now get the nbestpeaks
+        nbestperiods, nbestlspvals, peakcount = (
+            [finperiods[bestperiodind]],
+            [finlsp[bestperiodind]],
+            1
+        )
+        prevperiod = sortedlspperiods[0]
+
+        # find the best nbestpeaks in the lsp and their periods
+        for period, lspval in zip(sortedlspperiods, sortedlspvals):
+
+            if peakcount == nbestpeaks:
+                break
+            perioddiff = abs(period - prevperiod)
+            bestperiodsdiff = [abs(period - x) for x in nbestperiods]
+
+            # print('prevperiod = %s, thisperiod = %s, '
+            #       'perioddiff = %s, peakcount = %s' %
+            #       (prevperiod, period, perioddiff, peakcount))
+
+            # this ensures that this period is different from the last
+            # period and from all the other existing best periods by
+            # periodepsilon to make sure we jump to an entire different peak
+            # in the periodogram
+            if (perioddiff > periodepsilon and
+                all(x > periodepsilon for x in bestperiodsdiff)):
+                nbestperiods.append(period)
+                nbestlspvals.append(lspval)
+                peakcount = peakcount + 1
+
+            prevperiod = period
+
+
+        return {'bestperiod':finperiods[bestperiodind],
+                'bestlspval':finlsp[bestperiodind],
+                'nbestpeaks':nbestpeaks,
+                'nbestlspvals':nbestlspvals,
+                'nbestperiods':nbestperiods,
+                'lspvals':lsp,
+                'periods':periods,
+                'method':'aov',
+                'kwargs':{'startp':startp,
+                          'endp':endp,
+                          'stepsize':stepsize,
+                          'normalize':normalize,
+                          'phasebinsize':phasebinsize,
+                          'mindetperbin':mindetperbin,
+                          'autofreq':autofreq,
+                          'periodepsilon':periodepsilon,
+                          'nbestpeaks':nbestpeaks,
+                          'sigclip':sigclip}}
+
+    else:
+
+        LOGERROR('no good detections for these times and mags, skipping...')
+        return {'bestperiod':npnan,
+                'bestlspval':npnan,
+                'nbestpeaks':nbestpeaks,
+                'nbestlspvals':None,
+                'nbestperiods':None,
+                'lspvals':None,
+                'periods':None,
+                'method':'aov',
+                'kwargs':{'startp':startp,
+                          'endp':endp,
+                          'stepsize':stepsize,
+                          'normalize':normalize,
+                          'phasebinsize':phasebinsize,
+                          'mindetperbin':mindetperbin,
+                          'autofreq':autofreq,
+                          'periodepsilon':periodepsilon,
+                          'nbestpeaks':nbestpeaks,
+                          'sigclip':sigclip}}
