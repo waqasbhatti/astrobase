5e92346e452b895b5efef47346277275ed9b81cf
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-10-13 15:09:03 -0400

    v0.2.5

--- astrobase/oldhatlc.py
+++ astrobase/oldhatlc.py
@@ -18,7 +18,7 @@ curve format description.
 
 '''
 # put this in here because oldhatlc can be used as a standalone module
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 import os.path
 import gzip
