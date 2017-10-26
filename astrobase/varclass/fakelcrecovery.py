6265c3614781309bfd374c98d04ca1ea9e6e3d34
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-10-26 15:53:32 -0400

    varclass.recoverysim -> two modules: fakelcgen and fakelcrecovery

---
+++ astrobase/varclass/fakelcrecovery.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''fakelcrecovery - Waqas Bhatti (wbhatti@astro.princeton.edu) - Oct 2017
+License: MIT. See the LICENSE file for more details.
+
+This is a companion module for fakelcgen.py. It runs LCs generated using
+functions in that module through variable star detection and classification to
+see how well they are recovered.
+
+TODO: implement below
+
+calculate the various non-periodic variability indices for these sim LCs and
+test recall and precision. get the PCA PC1 of this for a combined var index.
+
+tune for a true positive and false positive rate using the ROC curve and set the
+sigma limit for confirmed variability per magnitude bin.
+
+afterwards check if successful by cross validation.
+
+run period searches and see recovery rate by period, amplitude, magnitude,
+number of observations, etc.
+
+'''
+import os
+import os.path
+import pickle
+
+import multiprocessing as mp
+import logging
+from datetime import datetime
+from traceback import format_exc
+from concurrent.futures import ProcessPoolExecutor
+from hashlib import md5
+
+# to turn a list of keys into a dict address
+# from https://stackoverflow.com/a/14692747
+from functools import reduce
+from operator import getitem
+def dict_get(datadict, keylist):
+    return reduce(getitem, keylist, datadict)
+
+import numpy as np
+import numpy.random as npr
+# seed the numpy random generator
+npr.seed(0xdecaff)
+
+import scipy.stats as sps
+import scipy.interpolate as spi
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+#############
+## LOGGING ##
+#############
+
+# setup a logger
+LOGGER = None
+
+def set_logger_parent(parent_name):
+    globals()['LOGGER'] = logging.getLogger('%s.fakelcrecovery' % parent_name)
+
+def LOGDEBUG(message):
+    if LOGGER:
+        LOGGER.debug(message)
+    elif DEBUG:
+        print('%sZ [DBUG]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGINFO(message):
+    if LOGGER:
+        LOGGER.info(message)
+    else:
+        print('%sZ [INFO]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGERROR(message):
+    if LOGGER:
+        LOGGER.error(message)
+    else:
+        print('%sZ [ERR!]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGWARNING(message):
+    if LOGGER:
+        LOGGER.warning(message)
+    else:
+        print('%sZ [WRN!]: %s' % (datetime.utcnow().isoformat(), message))
+
+def LOGEXCEPTION(message):
+    if LOGGER:
+        LOGGER.exception(message)
+    else:
+        print(
+            '%sZ [EXC!]: %s\nexception was: %s' % (
+                datetime.utcnow().isoformat(),
+                message, format_exc()
+                )
+            )
+
+
+###################
+## LOCAL IMPORTS ##
+###################
+
+import astrobase.lcproc as lcproc
+import astrobase.varbase.features as vfeatures
+
+
+#######################
+## LC FORMATS SET UP ##
+#######################
+
+def read_pklc(lcfile):
+    '''
+    This just reads a pickle LC.
+
+    '''
+
+    try:
+        with open(lcfile,'rb') as infd:
+            lcdict = pickle.load(infd)
+    except UnicodeDecodeError:
+        with open(lcfile,'rb') as infd:
+            lcdict = pickle.load(infd, encoding='latin1')
+
+    return lcdict
