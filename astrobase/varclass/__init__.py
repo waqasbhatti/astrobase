20b693c4bdf9c8b7ee76b8c2c725ea67c8f3d573
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-12-05 12:04:58 -0500

    varclass: add a periodicfeatures module

--- astrobase/varclass/__init__.py
+++ astrobase/varclass/__init__.py
@@ -7,6 +7,12 @@ License: MIT. See the LICENSE file for more details.
 This contains various modules that run variable star classification and
 characterize its reliability and completeness via simulating LCs.
 
-recoverysim.py - LC generation and recovery sims for completeness/reliability
+fakelcgen.py        - fake light curve generation and injection of variability
+fakelcrecovery.py   - recovery of fake light curve variability and periodic vars
+
+periodicfeatures.py - periodic variable light curve features
+
+rfclass.py          - random forest classifier and support functions for
+                      variability classification
 
 '''
