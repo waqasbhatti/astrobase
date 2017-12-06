8cf7a280d9fecfa968aaca881cbde09e6733ce06
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-12-06 12:31:30 -0500

    periodicfeatures: roll in periodic_ and lcfit_features into one fn

--- astrobase/varclass/periodicfeatures.py
+++ astrobase/varclass/periodicfeatures.py
@@ -15,6 +15,7 @@ import logging
 from datetime import datetime
 from traceback import format_exc
 from time import time as unixtime
+from itertools import combinations
 
 #############
 ## LOGGING ##
@@ -73,27 +74,144 @@ from ..varbase import lcfit
 ## FEATURE CALCULATION FUNCTIONS ##
 ###################################
 
-def fourier_features(times, mags, errs, period,
-                     fourierorder=8,
-                     sigclip=3.0,
-                     magsarefluxes=False):
+def lcfit_features(times, mags, errs, period,
+                   fourierorder=8,
+                   # these are depth, duration, ingress duration
+                   transitparams=[-0.01,0.1,0.1],
+                   # these are depth, duration, depth ratio, secphase
+                   ebparams=[-0.2,0.3,0.7,0.5],
+                   sigclip=10.0,
+                   magsarefluxes=False,
+                   verbose=True):
     '''
-    This calculates various ratios of fourier-fit amplitudes and phase params.
+    This calculates various features related to fitting models to light curves.
+
+    - calculates R_ij and phi_ij ratios for Fourier fit amplitudes and phases
+    - calculates the redchisq for fourier, EB, and planet transit fits
+    - calculates the redchisq for fourier, EB, planet transit fits w/2 x period
 
     '''
 
-    # freq_amplitude_ratio_21 - amp ratio of the 2nd to 1st Fourier component
+    #
+    # fourier fit
+    #
 
-    # freq_amplitude_ratio_31 - amp ratio of the 3rd to 1st Fourier component
+    # we fit a Fourier series to the light curve using the best period and
+    # extract the amplitudes and phases up to the 8th order to fit the LC. the
+    # various ratios of the amplitudes A_ij and the differences in the phases
+    # phi_ij are also used as periodic variability features
 
-    # freq_rrd - 1 if freq_frequency_ratio_21 or freq_frequency_ratio_31 are
-    #            close to 0.746 (characteristic of RRc? -- double mode RRLyr), 0
-    #            otherwise
+    # do the fit
+    ffit = lcfit.fourier_fit_magseries(times, mags, errs, period,
+                                       fourierorder=fourierorder,
+                                       sigclip=sigclip,
+                                       magsarefluxes=magsarefluxes,
+                                       verbose=verbose)
 
-    # in addition, we fit a Fourier series to the light curve using the best
-    # period and extract the amplitudes and phases up to the 8th order to fit
-    # the LC. the various ratios of the amplitudes A_ij and the differences in
-    # the phases phi_ij are also used as periodic variability features
+    # get the coeffs and redchisq
+    fourier_fitcoeffs = ffit['fitinfo']['finalparams']
+    fourier_chisq = ffit['fitchisq']
+    fourier_redchisq = ffit['fitredchisq']
+
+    # break them out into amps and phases
+    famplitudes = fourier_fitcoeffs[:fourierorder]
+    fphases = fourier_fitcoeffs[fourierorder:]
+
+    famp_combos = combinations(famplitudes,2)
+    famp_cinds = combinations(range(len(famplitudes)),2)
+
+    fpha_combos = combinations(fphases,2)
+    fpha_cinds = combinations(range(len(fphases)),2)
+
+    fampratios = {}
+    fphadiffs = {}
+
+    # get the ratios for all fourier coeff combinations
+    for ampi, ampc, phai, phac in zip(famp_cinds,
+                                      famp_combos,
+                                      fpha_cinds,
+                                      fpha_combos):
+
+        ampratind = 'R_%s%s' % (ampi[1]+1, ampi[0]+1)
+        # this is R_ij
+        amprat = ampc[1]/ampc[0]
+        phadiffind = 'phi_%s%s' % (phai[1]+1, phai[0]+1)
+        # this is phi_ij
+        phadiff = phac[1] - phai[0]*phac[0]
+
+        fampratios[ampratind] = amprat
+        fphadiffs[phadiffind] = phadiff
+
+    # update the outdict for the Fourier fit results
+    outdict = {'fourier_ampratios':fampratios,
+               'fourier_phadiffs':fphadiffs,
+               'fourier_fitparams':fourier_fitcoeffs,
+               'fourier_redchisq':fourier_redchisq,
+               'fourier_chisq':fourier_chisq}
+
+    # EB and planet fits will find the epoch automatically
+    planetfitparams = [period,
+                       None,
+                       transitparams[0],
+                       transitparams[1],
+                       transitparams[2]]
+
+    ebfitparams = [period,
+                   None,
+                   ebparams[0],
+                   ebparams[1],
+                   ebparams[2]]
+
+    # do the planet and EB fit with this period
+    planet_fit = lcfit.traptransit_fit_magseries(times, mags, errs,
+                                                 planetfitparams,
+                                                 sigclip=sigclip,
+                                                 magsarefluxes=magsarefluxes,
+                                                 verbose=verbose)
+
+    planetfit_finalparams = planet_fit['fitinfo']['finalparams']
+    planetfit_chisq = planet_fit['fitchisq']
+    planetfit_redchisq = planet_fit['fitredchisq']
+
+
+    eb_fit = lcfit.gaussianeb_fit_magseries(times, mags, errs,
+                                            ebfitparams,
+                                            sigclip=sigclip,
+                                            magsarefluxes=magsarefluxes,
+                                            verbose=verbose)
+
+    ebfit_finalparams = eb_fit['fitinfo']['finalparams']
+    ebfit_chisq = eb_fit['fitchisq']
+    ebfit_redchisq = eb_fit['fitredchisq']
+
+    # do the EB fit with 2 x period
+    ebfitparams[0] = ebfitparams[0]*2.0
+    eb_fitx2 = lcfit.gaussianeb_fit_magseries(times, mags, errs,
+                                              ebfitparams,
+                                              sigclip=sigclip,
+                                              magsarefluxes=magsarefluxes,
+                                              verbose=verbose)
+
+    ebfitx2_finalparams = eb_fitx2['fitinfo']['finalparams']
+    ebfitx2_chisq = eb_fitx2['fitchisq']
+    ebfitx2_redchisq = eb_fitx2['fitredchisq']
+
+    # update the outdict
+    outdict.update({
+        'planet_fitparams':planetfit_finalparams,
+        'planet_chisq':planetfit_chisq,
+        'planet_redchisq':planetfit_redchisq,
+        'eb_fitparams':ebfit_finalparams,
+        'eb_chisq':ebfit_chisq,
+        'eb_redchisq':ebfit_redchisq,
+        'ebx2_fitparams':ebfitx2_finalparams,
+        'ebx2_chisq':ebfitx2_chisq,
+        'ebx2_redchisq':ebfitx2_redchisq,
+    })
+
+
+    return outdict
+
 
 
 def periodogram_features(periodogramresults,
@@ -163,16 +281,19 @@ def phasedlc_features(times, mags, errs, period):
 
 
 
-def lcfit_features(times, mags, errs, period):
+def neighbor_features(lclistpkl,
+                      objectid,
+                      fwhm_arcsec,
+                      period,
+                      epoch):
     '''
-    This calculates various features related to fits to the phased LC.
+    This calculates various features based on neighboring stars.
 
     '''
 
-    # ebchisq, pltchisq, fourierchisq, splchisq - red-chisq-values for these
-    #                                             fits to the phased LC
+    # distance to closest neighbor in arcsec
 
-    # medperc90_2p_p - 90th percentile of the absolute residual values around
-    #                  the light curve phased with best period x 2 divided by
-    #                  the same quantity for the residuals using the phased
-    #                  light curve with best period (to detect EBs)
+    # number of neighbors within 2 x fwhm_arcsec
+
+    # sum of absdiff between the phased LC of this object and that of the
+    # closest neighbor phased with the same period and epoch
