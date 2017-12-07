bf507ee5ec58a94840464c0b86bd8eef3431adba
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-12-07 13:26:38 -0500

    moved fakelc stuff into its own fakelcs package

---
+++ astrobase/fakelcs/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''fakelcs - Waqas Bhatti (wbhatti@astro.princeton.edu) - Oct 2017
+License: MIT. See the LICENSE file for more details.
+
+This contains various modules that run variable star classification and
+characterize its reliability and completeness via simulating LCs.
+
+generation.py     - fake light curve generation and injection of variability
+recovery.py       - recovery of fake light curve variability and periodic vars
+trilegal.py       - interface to get data tables from the TRILEGAL galaxy model
+dust.py           - interface to get the SF11 and SFD Av coefficents from 2MASS
+
+'''
