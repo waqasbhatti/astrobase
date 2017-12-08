7cf345cb03b21d26df6cee56244d451c2a264ea6
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-12-08 12:22:48 -0500

    rfclass: plot_training_results

--- astrobase/varclass/rfclass.py
+++ astrobase/varclass/rfclass.py
@@ -27,6 +27,8 @@ import glob
 import os.path
 import os
 import shutil
+import itertools
+
 try:
     import cPickle as pickle
 except:
@@ -56,6 +58,10 @@ from sklearn.model_selection import train_test_split
 from operator import itemgetter
 from sklearn.metrics import r2_score, median_absolute_error, \
     precision_score, recall_score, confusion_matrix, f1_score
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 #############
 ## LOGGING ##
@@ -109,15 +115,17 @@ def LOGEXCEPTION(message):
 #######################
 
 # Utility function to report best scores
-def gridsearch_report(grid_scores, n_top=3):
-    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
-    for i, score in enumerate(top_scores):
-        LOGINFO("Model with rank: {0}".format(i + 1))
-        LOGINFO("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-              score.mean_validation_score,
-              np.std(score.cv_validation_scores)))
-        LOGINFO("Parameters: {0}".format(score.parameters))
-
+# modified from a snippet taken from:
+# http://scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search.html
+def gridsearch_report(results, n_top=3):
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            LOGINFO("Model with rank: {0}".format(i))
+            LOGINFO("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            LOGINFO("Parameters: {0}".format(results['params'][candidate]))
 
 
 
@@ -380,13 +388,12 @@ def train_rf_classifier(
 
     # this is the grid def for hyperparam optimization
     rf_hyperparams = {
-        "max_depth": [3,4,5,10,20,None],
+        "max_depth": [3,4,5,None],
         "n_estimators":sp_randint(100,2000),
         "max_features": sp_randint(1, 5),
         "min_samples_split": sp_randint(2, 11),
         "min_samples_leaf": sp_randint(2, 11),
     }
-
 
     # run the stratified kfold cross-validation on training features using our
     # random forest classifier object
@@ -407,7 +414,7 @@ def train_rf_classifier(
                                         training_labels)
 
     # report on the classifiers' performance
-    gridsearch_report(cvsearch_classifiers.grid_scores_)
+    gridsearch_report(cvsearch_classifiers.cv_results_)
 
     # get the best classifier after CV is done
     bestclf = cvsearch_classifiers.best_estimator_
@@ -421,7 +428,6 @@ def train_rf_classifier(
     precscore = precision_score(testing_labels,test_predicted_labels)
     f1score = f1_score(testing_labels, test_predicted_labels)
     confmatrix = confusion_matrix(testing_labels, test_predicted_labels)
-
 
     # write the classifier, its training/testing set, and its stats to the
     # pickle if requested
@@ -549,12 +555,85 @@ def apply_rf_classifier(classifier,
 ## PLOTTING RESULTS ##
 ######################
 
-def plot_training_results(classifier):
+def plot_training_results(classifier,
+                          classlabels,
+                          outfile):
     '''
     This plots the training results from the classifier run on the training set.
 
     - plots the confusion matrix
     - plots the feature importances
 
-
     '''
+
+    if isinstance(classifier,str) and os.path.exists(classifier):
+        with open(classifier,'rb') as infd:
+            clfdict = pickle.load(infd)
+    elif isinstance(classifier, dict):
+        clfdict = classifier
+    else:
+        LOGERROR("can't figure out the input classifier arg")
+        return None
+
+    confmatrix = clfdict['best_confmatrix']
+    overall_feature_importances = clfdict['best_classifier'].feature_importances_
+    feature_importances_per_tree = np.array([
+        tree.feature_importances_
+        for tree in clfdict['best_classifier'].estimators_
+    ])
+    stdev_feature_importances = np.std(feature_importances_per_tree,axis=0)
+
+    feature_names = np.array(clfdict['feature_names'])
+
+    plt.figure(figsize=(6.4*3.0,4.8))
+
+    # confusion matrix
+    plt.subplot(121)
+    classes = np.array(classlabels)
+    plt.imshow(confmatrix, interpolation='nearest', cmap=plt.cm.Blues)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+    plt.title('evaluation set confusion matrix')
+    plt.ylabel('predicted class')
+    plt.xlabel('actual class')
+
+    thresh = confmatrix.max() / 2.
+    for i, j in itertools.product(range(confmatrix.shape[0]),
+                                  range(confmatrix.shape[1])):
+        plt.text(j, i, confmatrix[i, j],
+                 horizontalalignment="center",
+                 color="white" if confmatrix[i, j] > thresh else "black")
+
+    # feature importances
+    plt.subplot(122)
+
+    features = np.array(feature_names)
+    sorted_ind = np.argsort(overall_feature_importances)[::-1]
+
+    features = features[sorted_ind]
+    feature_names = feature_names[sorted_ind]
+    overall_feature_importances = overall_feature_importances[sorted_ind]
+    stdev_feature_importances = stdev_feature_importances[sorted_ind]
+
+    plt.bar(np.arange(0,features.size),
+            overall_feature_importances,
+            yerr=stdev_feature_importances,
+            width=0.8,
+            color='grey')
+    plt.xticks(np.arange(0,features.size),
+               features,
+               rotation=90)
+    plt.yticks([0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
+    plt.xlim(-0.75, features.size - 1.0 + 0.75)
+    plt.ylim(0.0,0.9)
+    plt.ylabel('relative importance')
+    plt.title('relative importance of features')
+
+    plt.subplots_adjust(wspace=0.1)
+
+    plt.savefig(outfile,
+                bbox_inches='tight',
+                dpi=100)
+    plt.close('all')
+    return outfile
