312567c6f7d6706d7f3209592993da29152d87b0
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-12-12 18:11:02 -0500

    minor fixes

--- astrobase/fakelcs/__init__.py
+++ astrobase/fakelcs/__init__.py
@@ -10,6 +10,6 @@ characterize its reliability and completeness via simulating LCs.
 generation.py     - fake light curve generation and injection of variability
 recovery.py       - recovery of fake light curve variability and periodic vars
 trilegal.py       - interface to get data tables from the TRILEGAL galaxy model
-dust.py           - interface to get the SF11 and SFD Av coefficents from 2MASS
+                    also contains interface to the 2MASS DUST extinction service
 
 '''
