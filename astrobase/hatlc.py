47671574b010b92e5d53141103e0690e19ae25b9
Waqas Bhatti <wbhatti@astro.princeton.edu>
2018-01-10 18:04:58 -0500

    v0.3.2

--- astrobase/hatlc.py
+++ astrobase/hatlc.py
@@ -78,7 +78,7 @@ https://github.com/waqasbhatti/astrobase/blob/master/notebooks/lightcurve-work.i
 '''
 
 # put this in here because hatlc can be used as a standalone module
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 ####################
 ## SYSTEM IMPORTS ##
