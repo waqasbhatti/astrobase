62edc37f47cc1c38e27e04ae7f137e4e2e7372ff
Waqas Bhatti <wbhatti@astro.princeton.edu>
2018-01-26 14:38:13 -0500

    magnitudes: add GAIA absolute mag calc

--- astrobase/magnitudes.py
+++ astrobase/magnitudes.py
@@ -276,3 +276,57 @@ def jhk_to_sdssz(jmag, hmag, kmag):
 
 # R-I for r-i < 0.95 = 1.00(r-i) + 0.21
 # R-I for r-i > 0.95 = 0.70(r-i) + 0.49
+
+
+
+
+def absolute_gaia_magnitude(gaia_mag,
+                            gaia_parallax_mas,
+                            gaia_mag_err=None,
+                            gaia_parallax_err_mas=None):
+    '''Calculates the GAIA absolute magnitude for object (or array of objects).
+
+    Given a G mag and the parallax measured by GAIA, gets the absolute mag using
+    the usual equation:
+
+    G - M_G = 5 x log10(d_pc) - 5
+    M_G = 5 - 5log10(d_pc) + G
+
+    '''
+
+    # get the distance
+    # we're using the naive calculation of d. this is inaccurate as stated in
+    # Bailer-Jones 2015 (http://arxiv.org/abs/1507.02105) if the error in
+    # parallax is a significant fraction of parallax
+    d_pc = np.abs(1000.0/gaia_parallax_mas)
+
+    # get the distance error
+    if gaia_parallax_err_mas is not None:
+        d_pc_err = (
+            (1000.0/(gaia_parallax_mas*gaia_parallax_mas)) *
+            gaia_parallax_err_mas
+        )
+    else:
+        d_pc_err = None
+
+    # calculate the absolute mag from the relation
+    # FIXME: this is NOT corrected for extinction in G mag. see Jordi+ 2010
+    # (http://adsabs.harvard.edu/abs/2010A%26A...523A..48J) to figure out
+    # A_G/A_V as a function of (V-I)_0, then apply it here
+    M_G = 5 - 5.0*np.log10(d_pc) + gaia_mag
+
+    # calculate the err in M_G
+    if d_pc_err is not None and gaia_mag_err is not None:
+
+        M_G_err = np.sqrt(
+            ((5.0/(gaia_distance * np.log(10.0)))**2 * (d_pc_err)**2) +
+            gaia_mag_err*gaia_mag_err
+        )
+    else:
+        M_G_err = None
+
+
+    if M_G_err is not None:
+        return M_G, M_G_err
+    else:
+        return M_G
