cf00e8bfcf3355110635c2f0af29533679a557ae
Waqas Bhatti <wbhatti@astro.princeton.edu>
2018-03-27 14:25:15 -0400

    v0.3.11

--- astrobase/hatsurveys/oldhatlc.py
+++ astrobase/hatsurveys/oldhatlc.py
@@ -18,7 +18,7 @@ curve format description.
 
 '''
 # put this in here because oldhatlc can be used as a standalone module
-__version__ = '0.3.10'
+__version__ = '0.3.11'
 
 import os.path
 import gzip
--- astrobase/services/hatds.py
+++ astrobase/services/hatds.py
@@ -68,7 +68,7 @@ details.
 '''
 
 # put this in here because hatds can be used as a standalone module
-__version__ = '0.3.10'
+__version__ = '0.3.11'
 
 
 #############
