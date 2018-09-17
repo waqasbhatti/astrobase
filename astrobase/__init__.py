fd6c8f4c560092e75ec26e8d82aa41eb4af8f443
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2018-09-16 22:00:31 -0400

    v0.3.19

--- astrobase/__init__.py
+++ astrobase/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3.18'
+__version__ = '0.3.19'
--- astrobase/hatsurveys/oldhatlc.py
+++ astrobase/hatsurveys/oldhatlc.py
@@ -18,7 +18,7 @@ curve format description.
 
 '''
 # put this in here because oldhatlc can be used as a standalone module
-__version__ = '0.3.18'
+__version__ = '0.3.19'
 
 import os.path
 import gzip
