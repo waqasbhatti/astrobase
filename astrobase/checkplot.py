08bb1e06ce77c4692e8aeaa3791d385956f07c60
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2018-10-25 18:36:47 -0400

    checkplot: more careful update of cp objectinfo

--- astrobase/checkplot.py
+++ astrobase/checkplot.py
@@ -131,6 +131,7 @@ import base64
 import sys
 import hashlib
 import json
+from copy import deepcopy
 
 try:
     import cPickle as pickle
@@ -5249,7 +5250,7 @@ def update_checkplot_objectinfo(cpf,
     else:
         objecttags = None
 
-    varinfo = cpd['varinfo'].copy()
+    varinfo = deepcopy(cpd['varinfo'])
 
     if 'comments' in cpd and cpd['comments'] is not None:
         comments = cpd['comments'][::]
@@ -5278,6 +5279,198 @@ def update_checkplot_objectinfo(cpf,
                                     findercachedir=findercachedir,
                                     verbose=verbose)
 
+    #
+    # don't update neighbors or finder chart if the new one is bad
+    #
+    if (newcpd['finderchart'] is None and
+        cpd['finderchart'] is not None):
+        newcpd['finderchart'] = deepcopy(
+            cpd['finderchart']
+        )
+
+    if (newcpd['neighbors'] is None and
+        cpd['neighbors'] is not None):
+        newcpd['neighbors'] = deepcopy(
+            cpd['neighbors']
+        )
+
+    #
+    # if there's existing GAIA info, don't overwrite if the new objectinfo dict
+    # doesn't have any
+    #
+    if (('failed' in newcpd['objectinfo']['gaia_status'] or
+         ('gaiaid' in newcpd['objectinfo'] and
+          newcpd['objectinfo']['gaiaid'] is None)) and
+        'ok' in cpd['objectinfo']['gaia_status']):
+
+        newcpd['objectinfo']['gaia_status'] = deepcopy(
+            cpd['objectinfo']['gaia_status']
+        )
+        if 'gaiaid' in cpd['objectinfo']:
+            newcpd['objectinfo']['gaiaid'] = deepcopy(
+                cpd['objectinfo']['gaiaid']
+            )
+        newcpd['objectinfo']['gaiamag'] = deepcopy(
+            cpd['objectinfo']['gaiamag']
+        )
+        newcpd['objectinfo']['gaia_absmag'] = deepcopy(
+            cpd['objectinfo']['gaia_absmag']
+        )
+        newcpd['objectinfo']['gaia_parallax'] = deepcopy(
+            cpd['objectinfo']['gaia_parallax']
+        )
+        newcpd['objectinfo']['gaia_parallax_err'] = deepcopy(
+            cpd['objectinfo']['gaia_parallax_err']
+        )
+        newcpd['objectinfo']['gaia_pmra'] = deepcopy(
+            cpd['objectinfo']['gaia_pmra']
+        )
+        newcpd['objectinfo']['gaia_pmra_err'] = deepcopy(
+            cpd['objectinfo']['gaia_pmra_err']
+        )
+        newcpd['objectinfo']['gaia_pmdecl'] = deepcopy(
+            cpd['objectinfo']['gaia_pmdecl']
+        )
+        newcpd['objectinfo']['gaia_pmdecl_err'] = deepcopy(
+            cpd['objectinfo']['gaia_pmdecl_err']
+        )
+
+    if (not np.isfinite(newcpd['objectinfo']['gaia_neighbors']) and
+        np.isfinite(cpd['objectinfo']['gaia_neighbors'])):
+        newcpd['objectinfo']['gaia_neighbors'] = deepcopy(
+            cpd['objectinfo']['gaia_neighbors']
+        )
+    if (not np.isfinite(newcpd['objectinfo']['gaia_closest_distarcsec']) and
+        np.isfinite(cpd['objectinfo']['gaia_closest_distarcsec'])):
+        newcpd['objectinfo']['gaia_closest_distarcsec'] = deepcopy(
+            cpd['objectinfo']['gaia_closest_gmagdiff']
+        )
+    if (not np.isfinite(newcpd['objectinfo']['gaia_closest_gmagdiff']) and
+        np.isfinite(cpd['objectinfo']['gaia_closest_gmagdiff'])):
+        newcpd['objectinfo']['gaia_closest_gmagdiff'] = deepcopy(
+            cpd['objectinfo']['gaia_closest_gmagdiff']
+        )
+
+    if (newcpd['objectinfo']['gaia_ids'] is None and
+        cpd['objectinfo']['gaia_ids'] is not None):
+        newcpd['objectinfo']['gaia_ids'] = deepcopy(
+            cpd['objectinfo']['gaia_ids']
+        )
+    if (newcpd['objectinfo']['gaia_xypos'] is None and
+        cpd['objectinfo']['gaia_xypos'] is not None):
+        newcpd['objectinfo']['gaia_xypos'] = deepcopy(
+            cpd['objectinfo']['gaia_xypos']
+        )
+    if (newcpd['objectinfo']['gaia_mags'] is None and
+        cpd['objectinfo']['gaia_mags'] is not None):
+        newcpd['objectinfo']['gaia_mags'] = deepcopy(
+            cpd['objectinfo']['gaia_mags']
+        )
+    if (newcpd['objectinfo']['gaia_parallaxes'] is None and
+        cpd['objectinfo']['gaia_parallaxes'] is not None):
+        newcpd['objectinfo']['gaia_parallaxes'] = deepcopy(
+            cpd['objectinfo']['gaia_parallaxes']
+        )
+    if (newcpd['objectinfo']['gaia_parallax_errs'] is None and
+        cpd['objectinfo']['gaia_parallax_errs'] is not None):
+        newcpd['objectinfo']['gaia_parallax_errs'] = deepcopy(
+            cpd['objectinfo']['gaia_parallax_errs']
+        )
+    if (newcpd['objectinfo']['gaia_pmras'] is None and
+        cpd['objectinfo']['gaia_pmras'] is not None):
+        newcpd['objectinfo']['gaia_pmras'] = deepcopy(
+            cpd['objectinfo']['gaia_pmras']
+        )
+    if (newcpd['objectinfo']['gaia_pmra_errs'] is None and
+        cpd['objectinfo']['gaia_pmra_errs'] is not None):
+        newcpd['objectinfo']['gaia_pmra_errs'] = deepcopy(
+            cpd['objectinfo']['gaia_pmra_errs']
+        )
+    if (newcpd['objectinfo']['gaia_pmdecls'] is None and
+        cpd['objectinfo']['gaia_pmdecls'] is not None):
+        newcpd['objectinfo']['gaia_pmdecls'] = deepcopy(
+            cpd['objectinfo']['gaia_pmdecls']
+        )
+    if (newcpd['objectinfo']['gaia_pmdecl_errs'] is None and
+        cpd['objectinfo']['gaia_pmdecl_errs'] is not None):
+        newcpd['objectinfo']['gaia_pmdecl_errs'] = deepcopy(
+            cpd['objectinfo']['gaia_pmdecl_errs']
+        )
+    if (newcpd['objectinfo']['gaia_absolute_mags'] is None and
+        cpd['objectinfo']['gaia_absolute_mags'] is not None):
+        newcpd['objectinfo']['gaia_absolute_mags'] = deepcopy(
+            cpd['objectinfo']['gaia_absolute_mags']
+        )
+    if (newcpd['objectinfo']['gaiak_colors'] is None and
+        cpd['objectinfo']['gaiak_colors'] is not None):
+        newcpd['objectinfo']['gaiak_colors'] = deepcopy(
+            cpd['objectinfo']['gaiak_colors']
+        )
+    if (newcpd['objectinfo']['gaia_dists'] is None and
+        cpd['objectinfo']['gaia_dists'] is not None):
+        newcpd['objectinfo']['gaia_dists'] = deepcopy(
+            cpd['objectinfo']['gaia_dists']
+        )
+
+    #
+    # don't overwrite good SIMBAD info with bad
+    #
+    if ('failed' in newcpd['objectinfo']['simbad_status'] and
+        'ok' in cpd['objectinfo']['simbad_status']):
+        newcpd['objectinfo']['simbad_status'] = deepcopy(
+            cpd['objectinfo']['simbad_status']
+        )
+
+    if (newcpd['objectinfo']['simbad_nmatches'] is None and
+        cpd['objectinfo']['simbad_nmatches'] is not None):
+        newcpd['objectinfo']['simbad_nmatches'] = deepcopy(
+            cpd['objectinfo']['simbad_nmatches']
+        )
+    if (newcpd['objectinfo']['simbad_mainid'] is None and
+        cpd['objectinfo']['simbad_mainid'] is not None):
+        newcpd['objectinfo']['simbad_mainid'] = deepcopy(
+            cpd['objectinfo']['simbad_mainid']
+        )
+    if (newcpd['objectinfo']['simbad_objtype'] is None and
+        cpd['objectinfo']['simbad_objtype'] is not None):
+        newcpd['objectinfo']['simbad_objtype'] = deepcopy(
+            cpd['objectinfo']['simbad_objtype']
+        )
+    if (newcpd['objectinfo']['simbad_allids'] is None and
+        cpd['objectinfo']['simbad_allids'] is not None):
+        newcpd['objectinfo']['simbad_allids'] = deepcopy(
+            cpd['objectinfo']['simbad_allids']
+        )
+    if (newcpd['objectinfo']['simbad_distarcsec'] is None and
+        cpd['objectinfo']['simbad_distarcsec'] is not None):
+        newcpd['objectinfo']['simbad_distarcsec'] = deepcopy(
+            cpd['objectinfo']['simbad_distarcsec']
+        )
+    if (newcpd['objectinfo']['simbad_best_mainid'] is None and
+        cpd['objectinfo']['simbad_best_mainid'] is not None):
+        newcpd['objectinfo']['simbad_best_mainid'] = deepcopy(
+            cpd['objectinfo']['simbad_best_mainid']
+        )
+    if (newcpd['objectinfo']['simbad_best_objtype'] is None and
+        cpd['objectinfo']['simbad_best_objtype'] is not None):
+        newcpd['objectinfo']['simbad_best_objtype'] = deepcopy(
+            cpd['objectinfo']['simbad_best_objtype']
+        )
+    if (newcpd['objectinfo']['simbad_best_allids'] is None and
+        cpd['objectinfo']['simbad_best_allids'] is not None):
+        newcpd['objectinfo']['simbad_best_allids'] = deepcopy(
+            cpd['objectinfo']['simbad_best_allids']
+        )
+    if (newcpd['objectinfo']['simbad_best_distarcsec'] is None and
+        cpd['objectinfo']['simbad_best_distarcsec'] is not None):
+        newcpd['objectinfo']['simbad_best_distarcsec'] = deepcopy(
+            cpd['objectinfo']['simbad_best_distarcsec']
+        )
+
+
+    #
+    # update the objectinfo dict
+    #
     cpd.update(newcpd)
     cpd['objectinfo']['objecttags'] = objecttags
     cpd['comments'] = comments
