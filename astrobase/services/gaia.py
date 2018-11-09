5667b53997901385f8db0f019998c1367d01ee53
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2018-11-09 10:46:50 -0500

    gaia: remove broken refetch try

--- astrobase/services/gaia.py
+++ astrobase/services/gaia.py
@@ -813,32 +813,6 @@ def tap_query(querystr,
 
         tablefname = cachefname
 
-        # try to open the cached file to make sure it's OK
-        try:
-            infd = gzip.open(cachefname,'rb')
-            gaia_objlist = np.genfromtxt(
-                infd,
-                names=True,
-                delimiter=',',
-                dtype='U20,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8',
-                usecols=(0,1,2,3,4,5,6,7,8,9,10,11,12)
-            )
-
-        except Exception as e:
-
-            LOGEXCEPTION('could not read cached GAIA result file: %s, '
-                         'fetching from server again' % cachefname)
-
-            return tap_query(querystr,
-                             gaia_mirror=gaia_mirror,
-                             returnformat=returnformat,
-                             forcefetch=True,
-                             cachedir=cachedir,
-                             verbose=verbose,
-                             timeout=timeout,
-                             refresh=refresh,
-                             maxtimeout=maxtimeout)
-
     #
     # all done with retrieval, now return the result dict
     #
