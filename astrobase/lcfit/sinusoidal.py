a650b582b0fd2e1ad535314921a5074d9335cad1
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-06-01 18:50:38 -0400

    lcfit.sinusoidal: minor fixes

--- astrobase/lcfit/sinusoidal.py
+++ astrobase/lcfit/sinusoidal.py
@@ -134,27 +134,11 @@ def _fourier_chisq(fourierparams,
     return chisq
 
 
-def _fourier_residual(fourierparams,
-                      phase,
-                      mags):
-    '''
-    This is the residual objective function to be minimized by `scipy.leastsq`.
-
-    The parameters are the same as `_fourier_func` above.
-
-    '''
-
-    f = _fourier_func(fourierparams, phase, mags)
-    residual = mags - f
-
-    return residual
-
-
 def fourier_fit_magseries(
         times, mags, errs, period,
         fourierorder=None,
         fourierparams=None,
-        fix_period=False,
+        fix_period=True,
         sigclip=3.0,
         magsarefluxes=False,
         plotfit=False,
