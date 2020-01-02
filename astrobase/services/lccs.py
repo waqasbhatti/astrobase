684910a9cfaa11fee359efe6db540a4818675dc8
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2020-01-02 04:04:34 -0500

    lccs: fix log formatting bits

--- astrobase/services/lccs.py
+++ astrobase/services/lccs.py
@@ -46,7 +46,11 @@ __version__ = '0.4.3'
 #############
 
 import logging
-from astrobase import log_sub, log_fmt, log_date_fmt
+
+# the basic logging styles common to all astrobase modules
+log_sub = '{'
+log_fmt = '[{levelname:1.1} {asctime} {module}:{lineno}] {message}'
+log_date_fmt = '%y%m%d %H:%M:%S'
 
 DEBUG = False
 if DEBUG:
